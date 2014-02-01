4fe74ae1bf524f14c5f40e9dd66c9f79e23a1670
lnln <line.93.214@gmail.com>
2014-02-01 23:52:53 +0900

    Express を用いるように変更

--- node_sample.js
+++ node_sample.js
@@ -1,17 +1,18 @@
 var http = require('http');
-var fs   = require('fs');
-var ejs  = require('ejs');
+var fs = require('fs');
+var ejs = require('ejs');
 var util = require('util');
+var express = require('express');
 
-var hello = fs.readFileSync('./hello.ejs', 'utf-8');
+var app = express();
+app.engine('ejs', ejs.renderFile);
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+app.get('/', function(req, res) {
   template_params = {time_now: new Date().toString()}
-  res.write(ejs.render(hello, template_params));
-  res.end();
-}).listen(process.env.PORT, process.env.IP);
+  res.render('hello.ejs', template_params)
+});
 
+server = http.createServer(app).listen(process.env.PORT || 3000, process.env.IP);
 addr = server.address();
 console.log('Server running at ' + addr.address + ':' + addr.port);
  
